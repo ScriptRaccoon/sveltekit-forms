62781e26b3ff29002882cc091ccfe4a024036c30
Script Raccoon <scriptraccoon@gmail.com>
2025-04-18 00:56:19 +0200

    update intro text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,5 +7,10 @@
 <p>
 	This is a demonstration of how to leverage reactivity in SvelteKit apps without any
 	client-side JavaScript. The state is encoded in the URL. And the JavaScript logic, if
-	needed, is handled on the Server.
+	needed, is handled on the server inside of form actions.
+</p>
+
+<p>
+	You can turn off JavaScript in your browser and all the examples will still work. The
+	only difference is that the page will reload when you click a button or link.
 </p>
